54053ebc0c5444632cf8838ae830ba4644466059
chauneng <50446006+chauneng@users.noreply.github.com>
2021-11-23 22:34:03 +0900

    고슴도치실패

--- Week03/17.7569.py
+++ Week03/17.7569.py
@@ -4,42 +4,47 @@ from collections import deque
 M, N, H = map(int, stdin.readline().split())
 
 box = [[] for _ in range(H)]
-visited = [[] for _ in range(H)]
 
 for i in range(H):
     for _ in range(N):
         box[i].append(list(map(int, stdin.readline().split())))
-        visited[i].append([0]*M)
 
-dx = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
+dz = [-1, 1, 0, 0, 0, 0]
+dx = [0, 0, -1, 1, 0, 0]
+dy = [0, 0, 0, 0, -1, 1]
+queue = deque()
 
-# print(visited)
+for i in range(H):
+    for j in range(N):
+        for l in range(M):
+            if box[i][j][l] == 1:
+                queue.append((i, j, l))
 
 def bfs():
 
-    queue = deque()
-    queue.appendleft((0, 0, 0))
-
-    row, column, level = queue.popleft()
-    
-
-    days = 0
-    
     while queue:
-         
-        for i in box:
-            for j in i:
-                for k in j:
-                    if k == 1:
-                        visited[i][j][k] = 1
-                        for l in range(6):
-                            newRow = row + dx[l]
-                            newColumn = column +dy[l]
-                            newLevel = level +dz[l]
-                            if 0 <= newRow <= N-1 and 0 <= newColumn <= M-1 and 0 <= newLevel<= H-1 and visited[newRow][newColumn][newLevel] == 0:
-                                queue.append((i, j, k))
+        level, row, column, = queue.popleft() 
+
+        for l in range(6):
+            newLevel = level +dz[l]
+            newRow = row + dx[l]
+            newColumn = column + dy[l]
+            if 0 <= newRow < N and 0 <= newColumn < M and 0 <= newLevel < H and box[newLevel][newRow][newColumn] == 0:
+                box[newLevel][newRow][newColumn] = box[level][row][column] + 1
+                queue.append((newLevel, newRow, newColumn))
 
 bfs()
-print(visited)
+
+def answer():
+    ans = -1
+    for i in box:
+        for j in i:
+            for k in j:
+                if k == 0:
+                    return -1
+                ans = max(k, ans)
+
+    return ans-1
+
+
+print(answer())
