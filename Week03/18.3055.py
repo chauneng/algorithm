ddf607a0282b96caf91ef53274d64d3f878164c7
chauneng <50446006+chauneng@users.noreply.github.com>
2021-11-23 23:20:39 +0900

    고슴도치 2차실패

--- Week03/18.3055.py
+++ Week03/18.3055.py
@@ -1,5 +1,5 @@
 from sys import stdin
-from collections import deque
+import heapq
 
 R, C = map(int, stdin.readline().split())
 m = []
@@ -11,40 +11,36 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def bfs():
-    time = 0
+    time = 1
+    while queue:
 
-    while hQueue:
-        # if len(wQueue) == 0:
-        #     return 'KAKTUS'
-        wRow, wColumn = wQueue.popleft()
+        t, id, row, column = heapq.heappop(queue)
+    
         for i in range(4):
-            newWRow = wRow + dx[i]
-            newWColumn = wColumn + dy[i]
-            if 0 <= newWRow < R and 0 <= newWColumn < C and m[newWRow][newWColumn] == '.':
-                m[newWRow][newWColumn] = '*'
-                wQueue.append((newWRow, newWColumn))
-
-        hRow, hColumn = hQueue.popleft()
-        for i in range(4):
-            newHRow = hRow + dx[i]
-            newHColumn = hColumn + dy[i]
-            if 0 <= newHRow < R and 0 <= newHColumn <C:
-                if m[newHRow][newHColumn] == 'D':
+            nRow = row + dx[i]
+            nColumn = column + dy[i]
+            if not id and 0 <= nRow < R and 0 <= nColumn < C and m[nRow][nColumn] == '.':
+                m[nRow][nColumn] = '*'
+                heapq.heappush(queue, (time, 0, nRow, nColumn))
+    
+            if id and 0 <= nRow < R and 0 <= nColumn <C:
+                if m[nRow][nColumn] == 'D':
                     return time
-                elif m[newHRow][newHColumn] == '.':
-                    m[hRow][hColumn] = '.'
-                    m[newHRow][newHColumn] = 'S'
-                    hQueue.append((newHRow, newHColumn))
+                elif id and 0 <= nRow < R and 0 <= nColumn < C and m[nRow][nColumn] == '.':
+                    m[nRow][nColumn] = 'S'
+                    heapq.heappush(queue, (time, 1, nRow, nColumn))
         time += 1
     return 'KAKTUS'
 
-wQueue, hQueue = deque(), deque()
+queue = []
 
 for i in range(R):
     for j in range(C):
         if m[i][j] == '*':
-            wQueue.append((i, j))
+            m[i][j] = 0
+            queue.append((0, 0, i, j))
         elif m[i][j] == 'S':
-            hQueue.append((i, j))
+            m[i][j] = 0
+            queue.append((0, 1, i, j))
 
 print(bfs())
