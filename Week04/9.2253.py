ea464bb18204846c87e168da7069e0bc54c9e65b
chauneng <50446006+chauneng@users.noreply.github.com>
2021-11-30 17:15:37 +0900

    bfs

--- Week04/9.2253.py
+++ Week04/9.2253.py
@@ -1,17 +1,27 @@
 from sys import stdin
-from math import sqrt
+from collections import deque
 
 N, M = map(int, stdin.readline().split())
-SMALL = {int(stdin.readline()) for _ in range(M)}
+SMALL = set([int(stdin.readline()) for _ in range(M)])
+check = [[] for _ in range(N + 1)]
+dx = (-1, 0, 1)
+queue = deque()
+queue.append([0, 1, 0])
+ans = -1
+while queue:
+    speed, position, jump = queue.popleft()
+    for i in range(3):
+        newSpeed = speed + dx[i]
+        if newSpeed <= 0:
+            continue
+        new_pos = position + newSpeed
+        if new_pos == N:
+            ans = jump + 1 
+            print(ans)
+            exit(0)
+            
+        if new_pos not in SMALL and new_pos < N and newSpeed not in check[new_pos]: 
+            check[new_pos].append(newSpeed)
+            queue.append([newSpeed, new_pos, jump+1])
 
-dp = [[float('inf')]*(int(sqrt(2*N))+2) for _ in range(N+1)]
-dp[1][0] = 0
-
-for p in range(2, N+1):
-    if p in SMALL:
-        continue
-    for v in range(int(sqrt(2*p)+1)):
-        dp[p][v] = min(dp[p-v][v-1], dp[p-v][v], dp[p-v][v+1])+1
-
-ans = min(dp[N])
-print(ans) if ans < float('inf') else print(-1)
+print(ans)
